3ff8cf6eef3515ef2eccafd137887bd5609a3fa8
Allison <80896970+hlabi-mazela@users.noreply.github.com>
2023-02-22 14:08:41 +0200

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,7 +6,6 @@ int main() {
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
-    // calculating sum
     sum = number1 + number2;      
     
     printf("%d + %d = %d", number1, number2, sum);
