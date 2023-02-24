32138e7009b4513b4e0594384336171dbd54a607
hlabi-mazela <timmo636@gmail.com>
2023-02-24 10:14:15 -0800

    More functions

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
 
-/*
- * main - checks the code for Holberton School
+/**
+ * main - check the code.
+ *
  * Return: Always 0.
- * */
-
+ */
 int main(void)
 {
-	char c;
+    char c;
 
-	c = 'A';
-	print("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return(0);
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
 }
